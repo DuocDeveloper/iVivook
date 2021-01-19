0b824190e8622e0758aea09946cf21756bf87e6d
Joan Toro Ortiz <j.toroo@profesor.duoc.cl>
2021-01-19 20:40:38 -0300

    Implementación de modelos app libreria

--- base/settings.py
+++ base/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'z6l7-jmndtlo@v9chwzbwn81sphj#%w^61fk7h*$2q6^e51z=b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Autor
+from .models import Autor, Libro, Existencia, Galeria
 
 # Register your models here.
 admin.site.register(Autor)
+admin.site.register(Libro)
+admin.site.register(Existencia)
+admin.site.register(Galeria)
